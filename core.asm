bb132f21270cef34cc8edd08c55adeb90192877b
radionroman <32839299+radionroman@users.noreply.github.com>
2023-04-17 21:52:02 +0200

    newest

--- core.asm
+++ core.asm
@@ -8,7 +8,6 @@ section .bss
 section .text
 global core
 core:
-        lea     rdx, [rel spinlock]
         push    rbp
         push    rbx
         push    r12
@@ -18,12 +17,16 @@ core:
         mov     rbp, rsp
         mov     r12, rdi
         mov     r13, rsi
-        mov     r14, 0
+        lea     r14, [rel spinlock]
         lea     r15, [rel shared_values]
         lea     rbx, [rel shared_spinlocks]
-        mov     r9, 0
+
 .loop1:
+        mov     r9, 0
         mov     r9b, byte [r13]          ; Odczytany znak to '+',
+        inc     r13
+        test    r9b, r9b
+        jz      .loop1exit
         cmp     r9b, '+'
         je      .plus
         cmp     r9b, '-'
@@ -46,177 +49,124 @@ core:
         je      .P
         cmp     r9b, 'S'
         je      .S
-        cmp     r9b, '0'
-        jl      .loop1exit
-        cmp     r9b, '9'
-        jg      .loop1exit
         jmp     .liczba
 .plus:
         pop     r10
         add     [rsp], r10
-        jmp     .sub1
+        jmp     .loop1
 .minus:
         pop     rax
         neg     rax
         push    rax
-        jmp     .switchend
+        jmp     .loop1
 .mult:
         pop     rax
         pop     r10
-        imul    r10
+        imul    rax, r10
         push    rax
-        jmp     .sub1
+        jmp     .loop1
 .n:
         push    r12
-        jmp     .add1
+        jmp     .loop1
 .B:
         pop     rax
         pop     r10
         cmp     r10, 0
         push    r10
-        je      .sub1
+        je      .loop1
         add     r13, rax
-        jmp     .sub1
+        jmp     .loop1
 .C:
         pop     r10
-        jmp     .sub1
+        jmp     .loop1
 .D:
         pop     r10
         push    r10
         push    r10
-        jmp     .add1
+        jmp     .loop1
 .E:
         pop     rax
         pop     r10
         push    rax
         push    r10
-        jmp     .switchend
+        jmp     .loop1
 .G:
-;        test    r14, 1
-;        jz      .G2
-;        sub     rsp, 8
         mov     rdi, r12
+        test    rsp, 0x8
+        jz     .GEven
+        sub     rsp, 8
         call    get_value
-;        add     rsp, 8
+        add     rsp, 8
         push    rax
-        jmp     .switchend
-;.G2:
-;        call    get_value
-;        push     rax
-;        jmp     .switchend
+        jmp     .loop1
+.GEven:
+        call    get_value
+        push    rax
+        jmp     .loop1
 .P:
-
         pop     rsi
         mov     rdi, r12
+        test    rsp, 0x8
+        jz      .PEven
+        sub     rsp, 8
         call    put_value
-        jmp     .sub1
-;.P2:
-;
-;        pop     r13
-;        push    r10
-;        push    r11
-;        sub     rsp, 8
-;        call    put_value
-;        add     rsp, 8
-;        pop     r12
-;        pop     r13
-;        jmp     .sub1
+        add     rsp, 8
+        jmp     .loop1
+.PEven:
+        call    put_value
+        jmp     .loop1
 
 .S:
 
         pop     r10
         pop     r11
         cmp     r10,r12
-        je      .switchend  ; wants to swap with itself
-
-        ;here should be mutex
+        je      .loop1  ; wants to swap with itself
         mov     rcx, 1
 .mutex:
-print   "czekam 1:", r12
-        xchg    rcx, [rdx]
-        test    rcx,rcx
-        jnz     .mutex
-       ; print  "left mutex: ", qword[rdx]
-        cmp     qword[rbx + r10 * 8], 0
-        jne     .second
-        mov     qword[rbx + r12 * 8], 1
-        mov     qword[rdx], 0
-        mov     rcx, 1
+;print   "czekam 1:", r12
+        xor     rax, rax
+        lock cmpxchg [r14], rcx
+        jne     .mutex
+;print  "left mutex: ", qword[rdx]
+        mov     rcx, r10
+        inc     rcx
+        mov     rax, r12
+        inc     rax
+        cmp     qword[rbx + r10 * 8], rax
+        je      .second
+        mov     qword[rbx + r12 * 8], rcx
+        mov     qword[r14], 0
 .waitpartner:
-print   "czekam 2:", r12
-        xchg    rcx, [rbx + r12 * 8]
-        test    rcx, rcx
-        jnz     .waitpartner
+;print   "czekam 2:", r12
+        xor     rax,rax
+        lock cmpxchg [rbx + r12 * 8], rcx
+        jne     .waitpartner
         mov     qword[rbx + r12 * 8], 0
         mov     [r15 + r10 * 8], r11
         push    qword[r15 + r12 * 8]
         mov     qword[rbx + r10 * 8], 0
-        jmp     .switchend
-
+        jmp     .loop1
 .second:
-
         ;open mutex
-
         mov     [r15 + r10 * 8], r11
-        mov     qword[rbx + r12 * 8], 1
+        mov     qword[rbx + r12 * 8], rcx
         mov     qword[rbx + r10 * 8], 0
-        mov     qword[rdx], 0
-        mov     rcx, 1
+        mov     qword[r14], 0
 .waitfirst:
-print   "czekam 3:", r12
-        xchg    rcx, [rbx + r12 * 8]
-        test    rcx, rcx
-        jnz     .waitfirst
+;print   "czekam 3:", r12
+        xor     rax,rax
+        lock cmpxchg [rbx + r12 * 8], rcx
+        jne     .waitfirst
         mov     qword[rbx + r12 * 8], 0
         push    qword[r15 + r12 * 8]
-        jmp     .switchend
+        jmp     .loop1
 
-
-;.S:
-;        pop     r10
-;        pop     r11
-;        cmp     r10, r12
-;        je      .switchend
-;        mov     [r15 + r12 * 8], r11
-;        mov     r9, qword [rbx + r10 * 8]
-;        test    r9, r9
-;        jnz     .issecond
-;
-;.isfirst:
-;        mov     qword [rbx + r12 * 8], 1
-;        mov     ecx, 1
-;.lockwait:
-;        xchg    ecx, [rbx + r12 * 8]
-;        test    ecx, ecx
-;        jnz      .lockwait
-;
-;        push    qword [r15 + r10 * 8]
-;        mov     qword [rbx + r12 * 8], 0
-;        mov     qword [rbx + r10 * 8], 0
-;        jmp     .switchend
-;.issecond:
-;        push    qword [r15 + r10 * 8]
-;        mov     qword [rbx + r10 * 8], 0
-;        mov     qword [rbx + r12 * 8], 1
-;        mov     ecx, 1
-;.lock2wait:
-;        xchg    ecx, [rbx + r12 * 8]
-;        test    ecx, ecx
-;        jnz      .lock2wait
-;        mov     qword [rbx + r12 * 8], 1
-;        jmp     .switchend
 .liczba:
         sub     r9, '0'
         push    r9
-        jmp     .add1
-.sub1:
-        dec     r14
-        jmp     .switchend
-.add1:
-        inc     r14
-.switchend:
-        inc     r13
         jmp     .loop1
+
 .loop1exit:
         pop     rax
         mov     rsp, rbp
